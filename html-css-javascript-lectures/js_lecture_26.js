986d75a2aa062e0141169f165b559794ce2b6afd
HiDamienDesai <131928836+HiDhavalDesai@users.noreply.github.com>
2025-06-03 11:33:27 -0500

    working on lecture 27

--- html-css-javascript-lectures/js_lecture_26.js
+++ html-css-javascript-lectures/js_lecture_26.js
@@ -3,7 +3,7 @@
 
 let numbers = [1,2,3,4,5];
 let max = Math.max(...numbers);
-// We cannot do this for an array, but we can use the spread operator.
+// We cannot do this for an array, but we can use the spread operator and it does allow the numbers to be unpacked.
 
 let min = Math.min(...numbers)
 
@@ -16,16 +16,18 @@ console.log(min);
 let username = "Code God";
 let letters = [...username];
 
-
 console.log(letters);
+// This will break up the words we entered in username into individual letters because of the spread operator.
+
 
 // Then you can use the .join method to add things in-between the characters
-
 let join_letters = [...username].join("-");
 console.log(join_letters);
 
+// What this is doing is first, it is breaking the username up into individual letters and then it is adding the "-" between each character.
 
-// we will create a shallow copy of this array; it's a different data structure, but contains similar values
+
+// we will create a shallow copy of this array; it's a different data structure, but contains similar values; just means its creating a new object but isn't a duplicate.
 let fruits = ["apple","orange","banana"];
 let newFruits = [...fruits];
 
--- html-css-javascript-lectures/js_lecture_27.js
+++ html-css-javascript-lectures/js_lecture_27.js
@@ -1,19 +1,17 @@
 // Rest Parameters = (...rest) allows a function work with a variable number of arguments by bundling them into an array.
 //                  similar to spread = expands an array into separates elements
-//                  rest = bundles separates elements into an array.
+//                  rest = bundles separates elements into an array. (It does the opposite of spread)
 
 
-function openFridge(...foods){
+function OpenFridge(...foods){
     console.log(foods);
 }
-// foods is creating a new variable for all the food items, so when I call the array of food(1-4), it will list all the foods in an array.
-// Then you can use rest parameters, so that you can turn the entire thing into a list of strings, instead of an array.
-
+// by adding 3 dots at the beginning of the
 
 const food1 = "pizza";
 const food2 = "pasta";
 const food3 = "tacos";
-const food4 = "mash potatoes";
-const food5 = "Ramen";
+const food4 = "sushi";
+// we are first creating the variables, then we create the function on top.
 
-openFridge(food1,food2,food3,food4,food5);
+OpenFridge(food1,food2,food3,food4);
