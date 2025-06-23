f0b5743a5c1f6e443363055b9a457c69bd5ad089
HiDamienDesai <131928836+HiDhavalDesai@users.noreply.github.com>
2025-06-23 14:49:00 -0500

    adding password & dice project

---
+++ html-css-javascript-projects/password_generator.js
@@ -0,0 +1,9 @@
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "~!@#$%^&*()_+{}|\:;<.,>?/-.";
+
+
+function generatePassword(){
+
+}
